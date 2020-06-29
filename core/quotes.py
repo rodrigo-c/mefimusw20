31d150d8e50946b1c505f824839bd5ec09773d54
rodrigo <rodrigo@areaweb.cl>
2020-06-28 23:45:32 -0400

    sdasd§

--- core/quotes.py
+++ core/quotes.py
@@ -67,4 +67,88 @@ quotes = '''
 “This will be our reply to violence: to make music more intensely, more beautifully, more devotedly than ever before.” ― Leonard Bernstein
 “Music is what tell us that the human race is greater than we realize.” ― Napoléon Bonaparte
 “If you were music, I would listen to you ceaselessly, and my low spirits would brighten up.” ― Anna Akhmatova
-“I would rather write 10,000 notes than a single letter of the alphabet.” ― Ludwig van Beethoven'''.splitlines()[1:]
+"Looking back on the production of 'Nevermind,' I'm embarrassed by it now. It's closer to a Motley Crue record than it is a punk rock record." - Kurt Cobain
+“I would rather write 10,000 notes than a single letter of the alphabet.” ― Ludwig van Beethoven
+“If music be the food of love, play on.” – William Shakespeare
+“Life seems to go on without effort when I am filled with music.” – George Eliot
+“Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything.” – Plato
+“After silence, that which comes nearest to expressing the inexpressible is music.” – Aldous Huxley
+“The music is not in the notes, but in the silence in between.” – Wolfgang Amadeus Mozart
+“Music, once admitted to the soul, becomes a sort of spirit and never dies.” – Edward Bulwer-Lytton
+“When I hear music, I fear no danger. I am invulnerable. I see no foe. I am related to the earliest of times, and to the latest.” – Henry David Thoreau
+“Music can name the unnameable and communicate the unknowable.” – Leonard Bernstein
+“Music in the soul can be heard by the universe.” – Lao Tzu
+“If you have to ask what jazz is, you’ll never know.” – Louis Armstrong
+“Do it again on the next verse and people think you meant it.” – Chet Atkins
+“Music is powerful. As people listen to it, they can be affected. They respond.” – Ray Charles
+“Music — what a powerful instrument, what a mighty weapon!” – Maria August von Trapp
+“Music is the strongest form of magic.” – Marilyn Manson
+“Without music, life would be a mistake.” – Friedrich Nietzsche
+“I merely took the energy it takes to pour and wrote some blues.” – Duke Ellington
+“Where words leave off, music begins.” – Heinrich Heine
+“I haven’t understood a bar of music in my life, but I have felt it.” – Igor Stravinsky
+“My idea is that there is music in the air, music all around us; the world is full of it, and you simply take as much as you require.” – Edward Elgar
+“You have to, take a deep breath. and allow the music to flow through you. Revel in it, allow yourself to awe. When you play allow the music to break your heart with its beauty.” – Kelly White
+“I don’t sing because I’m happy; I’m happy because I sing.” – William James
+“Music is your own experience, your thoughts, your wisdom. If you don’t live it, it won’t come out of your horn.” – Charlie Parker
+“A painter paints pictures on canvas. But musicians paint their pictures on silence. ” – Leopold Stokowski
+“Everything in the universe has a rhythm, everything dances.” – Maya Angelou
+“Music is the moonlight in the gloomy night of life.” Jean Paul Friedrich Richter
+“Music drives you. It wakes you up, it gets you pumping. And, at the end of the day, the correct tune will chill you down.” – Dimebag Darrell
+“Our sweetest songs are those that tell of saddest thought.” Percy Bysshe Shelley
+“I adore art… when I am alone with my notes, my heart pounds and the tears stream from my eyes, and my emotion and my joys are too much to bear.” – Guiseppe Verdi
+“Mournful and yet grand is the destiny of the artist.” – Franz Liszt
+“The musician is perhaps the most modest of animals, but he is also the proudest.” – Erik Satie
+“There is music in the air, music all around us; the world is full of it, and you simply take as much as you require.” – Edward Elgar
+“To send light into the darkness of men’s hearts – such is the duty of the artist.” – Robert Schumann
+“A creative artist works on his next composition because he was not satisfied with his previous one.” – Dmitri Shostakovich
+“I can’t understand why people are frightened of new ideas. I’m frightened of the old ones.” – John Cage
+“As long as we live, there is never enough singing.” – Martin Luther
+“Music comes to me more readily than words.” – Ludwig Van Beethoven
+“The only love affair I have ever had was with music.” – Maurice Ravel
+“Music brings a warm glow to my vision, thawing mind and muscle from their endless wintering.” – Haruki Murakami
+“Music is the divine way to tell beautiful, poetic things to the heart.” – Pablo Casals
+“Music produces a kind of pleasure which human nature cannot do without.” – Confucius
+“Music is the universal language of mankind.” – Henry Wadsworth Longfellow
+“Music is enough for a lifetime, but a lifetime is not enough for music.” – Sergei Rachmaninoff
+“Music is the only thing I’ve ever known that doesn’t have any rules at all.” – Josh Homme
+“How is it that music can, without words, evoke our laughter, our fears, our highest aspirations?” Jane Swan
+“Music expresses feeling and thought, without language; it was below and before speech, and it is above and beyond all words.” – Robert G. Ingersoll
+“Works of art make rules; rules do not make works of art.” – Claude Debussy
+“Wake up, live your life and sing the melody of your soul.” – Amit Ray
+“The easiest way to avoid wrong notes is to never open your mouth and sing. What a mistake that would be.” – Pete Seeger
+“Some days there won’t be a song in your heart. Sing anyway.” – Emory Austin
+“Music is the tool to express life – and all that makes a difference.” – Herbie Hancock
+“I would teach children music, physics, and philosophy; but most importantly music, for the patterns in music and all the arts are the keys to learning.”― Plato
+“Music gives a soul to the universe, wings to the mind, flight to the imagination
+and life to everything.”― Plato
+“Musical training is a more potent instrument than any other, because rhythm and harmony find their way into the inward places of the soul.”― Plato, The Republic
+“Without music, life would be a mistake.”― Friedrich Nietzsche, Twilight of the Idols
+“Music expresses that which cannot be put into words and that which cannot remain silent.”― Victor Hugo
+“Virtually every writer I know would rather be a musician.”― Kurt Vonnegut
+“Music is what tells us that the human race is greater than we realize.”― Napoleon Bonaparte
+“If one should desire to know whether a kingdom is well governed, if its morals are good or bad, the quality of its music will furnish the answer.”― Confucius
+“Music produces a kind of pleasure which human nature cannot do without.”― Confucius
+“If I were not a physicist, I would probably be a musician. I often think in music. I live my daydreams in music. I see my life in terms of music.”― Albert Einstein
+“After silence, that which comes nearest to expressing the inexpressible is music.”― Aldous Huxley, Music at Night and Other Essays
+“The only truth is music.”― Jack Kerouac
+“If I had my life to live over again, I would have made a rule to read some poetry and listen to some music at least once every week.”― Charles Darwin
+“Music is the universal language of mankind.”― Henry Wadsworth Longfellow
+“Where words fail, music speaks.”― Hans Christian Andersen
+“Life is like a beautiful melody, only the lyrics are messed up.”― Hans Christian Andersen
+“Music is the language of the spirit. It opens the secret of life bringing peace, abolishing strife.”― Kahlil Gibran
+“Where words leave off, music begins.”― Heinrich Heine
+“Music in the soul can be heard by the universe.”― Lao Tzu
+“When I hear music, I fear no danger. I am invulnerable. I see no foe. I am related to the earliest times, and to the latest.”― Henry David Thoreau
+“Music is the literature of the heart; it commences where speech ends.”― Alphonse de Lamartine
+“Without music, life would be a blank to me.”― Jane Austen, Emma
+“You couldn’t not like someone who liked the guitar.”― Stephen King, The Stand
+“There is no feeling, except the extremes of fear and grief, that does not find relief in music.”― George Eliot
+“The most exciting rhythms seem unexpected and complex, the most beautiful melodies simple and inevitable.”― W.H. Auden
+“I would say that music is the easiest means in which to express, but since words are my talent, I must try to express clumsily in words what the pure music would have done better.”― William Faulkner
+“With the truth, all given facts harmonize; but with what is false, the truth soon hits a wrong note.”― Aristotle, The Nicomachean Ethics
+“Music is the shorthand of emotion.”― Leo Tolstoy
+“Hell is full of musical amateurs.”― George Bernard Shaw
+'''.splitlines()[1:]
+
+quotes=list(set(quotes))
