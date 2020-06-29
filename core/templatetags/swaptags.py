7041cad4df6783302a0971d894846bfabe6850db
rodrigo <rodrigo@areaweb.cl>
2020-06-28 23:36:33 -0400

    sdasd§

--- core/templatetags/swaptags.py
+++ core/templatetags/swaptags.py
@@ -1,4 +1,7 @@
 from django import template
+from random import choice
+
+from core.quotes import quotes
 
 register = template.Library()
 
@@ -6,3 +9,9 @@ register = template.Library()
 @register.filter
 def class_name(value):
     return value.__class__.__name__
+
+
+@register.simple_tag
+def random_quote():
+    return choice(quotes)
+
